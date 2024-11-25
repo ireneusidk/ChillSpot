cff959e2f75a600a088c20b7da8faa684aa7f6f8
Ireneus_idk <k-my99@hotmail.com.ar>
2024-11-25 16:11:56 -0300

    Update api.jsx

--- front/src/componentes/api/api.jsx
+++ front/src/componentes/api/api.jsx
@@ -6,7 +6,7 @@ baseURL: 'https://chillspot-84lu.onrender.com/api', // En desarrollo
 });
 const getUserProfile = async () => {
   try {
-    const response = await api.get('/users/profile');
+    const response = await api.get('/users/protegida');
     console.log('Perfil del usuario:', response.data);
   } catch (error) {
     console.error('Error al obtener el perfil:', error);
