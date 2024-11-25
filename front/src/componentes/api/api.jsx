9f6520dc4d1e2733d2229dde40de84d1b1c9924b
Ireneus_idk <k-my99@hotmail.com.ar>
2024-11-25 12:42:00 -0300

    Update api.jsx

--- front/src/componentes/api/api.jsx
+++ front/src/componentes/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.138:8080/api' || 'https://chillspot-84lu.onrender.com',
+  baseURL: 'http://192.168.0.138:8080/api' || 'https://chillspot-84lu.onrender.com/api',
   withCredentials: true
 });
 const getUserProfile = async () => {
