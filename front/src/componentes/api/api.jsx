5a20c0690273312e8f4e7269837c6fb170ad251d
Ireneus_idk <k-my99@hotmail.com.ar>
2024-11-25 13:11:15 -0300

    Update api.jsx

--- front/src/componentes/api/api.jsx
+++ front/src/componentes/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 baseURL: process.env.NODE_ENV === 'production'
-  ? 'https://chillspot-84lu.onrender.com/api' // En producción
+  ? 'https://chillspot-front.onrender.com' // En producción
   : 'http://192.168.0.138:8080/api',          // En desarrollo
   withCredentials: true
 });
